d1f8dfac37ee1f53d827329b4c607e6d0bc8c160
Afsah ur Rehman <afsahurrehman11@gmail.com>
2024-09-23 00:46:53 +0500

    removed all the hardcored text in HTML
    - made a json object of all the text

---
+++ script.js
@@ -0,0 +1,82 @@
+const person =
+{
+
+    name: "Afsah ur Rehman",
+
+    description: "Cloud (AWS, Azure) || Data Engineering || MERN",
+
+    hardSkills: 
+    [
+        "Cloud Azure", "Data Engineering", "React", "Node.js",
+        "Git & GitHub", "Angular", "MongoDB", "C/C++",
+        "MySQL", "JavaScript", "Python", "HTML, CSS, Bootstrap"
+    ],
+
+
+    
+    softSkills: [
+        "Problem Solving", "Communication", "Teamwork"
+    ],
+    
+    education: "BSCS from ITU (2022-2026)",
+
+
+    experience:
+    [
+        {
+            title: "DigiFloat",
+            period: "June 2024 - Aug 2024",
+            skills: "Cloud Computing, Data Engineering, Microsoft Azure, Generative AI, Python with PySpark"
+        },
+        {
+            title: "Web Development Intern, TIERS Limited",
+            period: "Jun 2023 – Aug 2023",
+            skills: "React, MySQL, Node.js, Bootstrap"
+        },
+        {
+            title: "Machine Learning Intern, Codesoft",
+            period: "Aug 2023 – Nov 2023",
+            skills: "Python with Scikit, Pandas, NumPy"
+        },
+        {
+            title: "Campus Ambassador, Search O Pal",
+            period: "Jun 2024 – present",
+            location: "Lahore, Pakistan"
+        }
+    ],
+    
+    projects: [
+        {
+            title: "Track Tracer",
+            description: "Music app with playlist creation and artist/album exploration. MERN stack: MongoDB, Node.js, Express"
+        },
+        {
+            title: "Search Engine",
+            description: "Rapidly scans 300,000+ files, performs search operations with advanced filters"
+        },
+        {
+            title: "Paint",
+            description: "MS Paint replica with Python and Tkinter"
+        },
+        {
+            title: "DOS Shell",
+            description: "MS-DOS inspired shell using C++ and dynamic data structures"
+        },
+        {
+            title: "Excel-Lite",
+            description: "Excel-like app using C++ and SFML for UI"
+        },
+        {
+            title: "2D C++ Games",
+            description: "Snake game, Solitaire, Shogi, Ludo, Chess: SFML for the UI"
+        }
+    ],
+
+
+    hackathons: [
+        {
+            name: "CodeRush 2024",
+            organization: "Organized by ITU"
+        }
+    ]
+};
